be70a22d0598fce8adbac34ed57e95d4aceebffa
guilherme <guilhermeserafin@gmail.com>
2023-12-02 17:10:11 -0300

    Finalizada parte da geracao da planilha e consertado css da pagina de login

--- projeto/public/controller/PedidoController.php
+++ projeto/public/controller/PedidoController.php
@@ -4,6 +4,8 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+use Database\ConnectionController as DB;
+
 //Classe do pedido INDIVIDUAL
 class PedidoController{
 
@@ -26,25 +28,62 @@ class PedidoController{
 
     public function gerarPedido()
     {
-        var_dump($_POST);
+        ini_set('max_execution_time', 300);
+        ini_set('memory_limit', '1G');
 
+        $descricoes = array();
         
+        foreach ($_POST as $key => $descricao) {
+            if (substr($key, 0, 5) != "valor") {
+                $descricoes[] = $descricao;
+            }
+        }
 
-        // ini_set('max_execution_time', 300);
-        // ini_set('memory_limit', '1G');
+        $placeholders = implode(',', array_fill(0, count($descricoes), '?'));
 
-        // $spreadsheet = IOFactory::load('teste.xlsx');
-        // $worksheet = $spreadsheet->getActiveSheet();
-        // $worksheet->getCell('G8')->setValue('2');
-        // $worksheet->getCell('G9')->setValue('abacate');
-        // $worksheet->getCell('G10')->setValue('3');
+        $conn = DB::connectDb();
 
-        // $writer = IOFactory::createWriter($spreadsheet, 'Xls');
+        $sql = "SELECT * FROM produto WHERE `sipac` IN ($placeholders)";
 
-        // header('Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        // header('Content-Disposition: attachment; filename="pedido.xlsx"');
+        $stmt = $conn->prepare($sql);
 
-        // $writer->save('pedido.xlsx');
+        foreach ($descricoes as $index => $descricao) {
+            $stmt->bindValue(($index + 1), $descricao); // Os índices dos placeholders no PDO começam em 1
+        }
+
+        if ($stmt->execute()) {
+            $itens = $stmt->fetchAll();
+        }
+
+        $spreadsheet = IOFactory::load('teste.xlsx');
+
+        $worksheet = $spreadsheet->getActiveSheet();
+
+        foreach($itens as $key => $item){
+
+            $linha = strval(8 + $key);
+
+            $worksheet->getCell('A' . $linha)->setValue($item['sipac']);
+            $worksheet->getCell('B' . $linha)->setValue($item['catmat_catser']);
+            $worksheet->getCell('C' . $linha)->setValue($item['natureza_despesa']);
+            $worksheet->getCell('D' . $linha)->setValue($key + 1);
+            $worksheet->getCell('E' . $linha)->setValue($item['item_pe']);
+            $worksheet->getCell('F' . $linha)->setValue($item['unidade_medida']);
+            $worksheet->getCell('G' . $linha)->setValue($item['descricao']);
+            $worksheet->getCell('H' . $linha)->setValue($_POST['valor' . strval($key + 1)]);
+
+        }
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+
+        // Configurar cabeçalhos HTTP para forçar o download
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="write.xlsx"');
+        header('Cache-Control: max-age=0');
+
+        // Envie o conteúdo do arquivo para a saída
+        $writer->save('php://output');
+
     }
 
     public function historicoPedidoIndividual($siape) {
